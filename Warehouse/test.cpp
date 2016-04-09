5bb1da7e7686abce658693e3de3bb0ec8500f67b
Jakub Krzywda <qbakrzywda@gmail.com>
2016-04-09 18:51:46 +0200

    Dodano projekt eclipse oraz schemat programu w UML

---
+++ Warehouse/test.cpp
@@ -0,0 +1,17 @@
+/*
+ * test.cpp
+ *
+ *  Created on: 9 kwi 2016
+ *      Author: Kuba
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout<<"Hello World!"<<endl;
+	return 0;
+}
+
+
